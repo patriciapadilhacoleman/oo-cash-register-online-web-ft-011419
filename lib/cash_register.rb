70ab9747251a1be2a1d868bbd9f34c94c1f04856
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-27 21:50:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,19 @@
 class CashRegister
   
-  def total
+  @@total = 0
+  
+  def self.total
     
+    @@total
   end
   
   def add_item
     
   end
+  
+  def apply_discount
+    
+  end
+  
   
 end
